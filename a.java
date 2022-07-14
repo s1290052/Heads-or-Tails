0485f5c952e3dc69e0dd8d1d9ed796956501738b
s1290052 <kotahigasi@gmail.com>
2022-07-14 11:54:48 +0900

    first commit

---
+++ a.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+    import java.util.Scanner;
+
+    public class a{
+
+    public static void main(String[] args) {
+
+        String[] hands = {"Heads", "Tails"};
+        Random ran = new Random();
+        Scanner sc = new Scanner(System.in);
+        int cou = 0;
+
+
+        System.out.println("Tossing a coin,,,");
+
+        for(int n = 0;n < 3;n++){
+            String point = hands[ran.nextInt(2)];
+            int t = n + 1;
+
+            if(point == hands[0])cou++;
+            System.out.println("Round "+t+": "+point);
+        }
+
+    int w = 3 - cou;
+    System.out.println("Heads: "+cou+", Tails: "+w);
+
+  
+
+    }
+    }
+
